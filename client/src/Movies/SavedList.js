7930b7a0113dd789a943b5650149b47f25dbab38
flowersfaerie <flowersfaerie@gmail.com>
2020-02-15 12:20:56 -0800

    Completed Part 2. Will work on some of the stretch goals.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -7,7 +7,7 @@ const SavedList = props => (
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <Link to="./Home">
+    <Link to="/">
     <div className="home-button">Home</div>
     </Link>
   </div>
